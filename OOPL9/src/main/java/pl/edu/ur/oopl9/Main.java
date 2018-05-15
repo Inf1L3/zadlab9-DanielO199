7df70e2984a8173835a4712df55fbbd1130f5d64
XxX <XxX@XxX-Komputer>
2018-05-15 20:52:05 +0200

    Zadanie 4

--- OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
+++ OOPL9/src/main/java/pl/edu/ur/oopl9/Main.java
@@ -1,15 +1,42 @@
 package pl.edu.ur.oopl9;
 
-import java.util.Random;
-import java.util.Iterator;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
+        
+        HashMap<String, String> slownik = new HashMap<>();
+        Scanner input=new Scanner(System.in);
+        Slowa slowa1=new Slowa();
+        slowa1.Elo(slownik);
+        String a=" ";
+        
+        while(!a.equals("koniec"))
+           {
+             
+              System.out.println("Podaj słowo ktore chcesz przetlumaczyc:");
+              a = input.nextLine();
+              
+               
+               
+              if(!slownik.containsKey(a))
+              {
+                  System.out.println("Takiego slowa nie ma w slowniku ");
+             
+              }
+              else
+                    System.out.println(slownik.get(a));
+               System.out.println("Jesli chcesz zakonczyc napisz koniec");
+          } 
+
+
+
+
+
         
        
 
--- OOPL9/src/main/java/pl/edu/ur/oopl9/Slowa.java
+++ OOPL9/src/main/java/pl/edu/ur/oopl9/Slowa.java
@@ -5,10 +5,45 @@
  */
 package pl.edu.ur.oopl9;
 
+import java.util.HashMap;
+
 /**
  *
  * @author XxX
  */
 public class Slowa {
-     public void elo(){}
+     
+    
+
+ public void Elo(HashMap<String, String> slownik) {
+
+        slownik.put("byc", "be");
+        slownik.put("otrzymac", "get");
+        slownik.put("dzien", "day");
+        slownik.put("umowa", "deal");
+        slownik.put("obiad", "dinner");
+        slownik.put("obowiazek", "duty");
+        slownik.put("miec", "have");
+        slownik.put("sloik", "jar");
+        slownik.put("praca", "job");
+        slownik.put("dodawac", "add");
+        slownik.put("wiek", "age");
+        slownik.put("tata", "father");
+        slownik.put("mama", "mother");
+        slownik.put("syn", "son");
+        slownik.put("wujek", "uncle");
+        slownik.put("samochod", "car");
+        slownik.put("woda", "water");
+        slownik.put("stol", "table");
+        slownik.put("telefon", "mobile phone");
+        slownik.put("buty", "shoes");
+
+    }
+
+
+
+
+
+
+    
 }
